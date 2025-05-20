d92c5722094180abd9da2543e1d267ca50c204f4
Aman Maharjan <neutrino2039@gmail.com>
2025-05-20 18:27:23 +0545

    Added header field to gify axios object in the backend

--- backend/src/controllers/api.ts
+++ backend/src/controllers/api.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const gify = axios.create({
   baseURL: "http://api.giphy.com/v1/gifs/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export { gify };
